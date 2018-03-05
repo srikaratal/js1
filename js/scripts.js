3af0757d3cf4da61dc482ce910480014bada3700
Piotr Wronarowicz <gawrone@interia.pl>
2018-03-05 21:22:36 +0100

    Add comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
 // scripts.js
 
 var name = prompt('Enter your name');
-alert('Hello, ' + name);
+// alert('Hello, ' + name);
 console.log('Hello, ' + name);
+/*
+    secret message
+*/
+// finally a task I can do with ease
