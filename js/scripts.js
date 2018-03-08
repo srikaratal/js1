62c748abe99299ee44f8aebb1b510bf2a5e554e6
Piotr Wronarowicz <gawrone@interia.pl>
2018-03-08 19:49:33 +0100

    Attempt 9.2

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,11 @@
 // scripts.js
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+var allNames = femaleNames.contact(maleNames);
+console.log(allNames);
+var indexOfOla = names.indexOf('Ola');
+var newName = 'Marian'
+if (allNames.indexOf(newName) === -1) {
+    var allNamesNew allNames.push(newName);
+}
 
-function getTriangleArea(a, h) {
-if (a <= 0) {
-    document.write("Nieprawidłowe dane");
-} else if (h <= 0) {
-    document.write("Nieprawidłowe dane");
-} else { return a*h/2;
-}
-}
-console.log(getTriangleArea(10, 6));
-var triangle1Area = getTriangleArea(10, 15);
-var triangle2Area = getTriangleArea(9, 31);
-var triangle3Area = getTriangleArea(16, 16);
