61b9c49231208fb17f217e9c4ce79efdfe656546
Piotr Wronarowicz <gawrone@interia.pl>
2018-03-08 23:05:22 +0100

    9.2

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,12 @@
 // scripts.js
 var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
 var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
-var allNames = femaleNames.contact(maleNames);
-console.log(allNames);
-var indexOfOla = names.indexOf('Ola');
-var newName = 'Marian'
-if (allNames.indexOf(newName) === -1) {
-    var allNamesNew allNames.push(newName);
-}
+var allNames = femaleNames.concat(maleNames);
 
+var newName = 'Marian'
+var indexOfMarian = allNames.indexOf(newName);
+
+if (indexOfMarian === -1) {
+    allNames.push(newName);
+};
+console.log(allNames);
