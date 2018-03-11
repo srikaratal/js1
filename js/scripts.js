e7c4e7764d20f253302f05289e6765a0821473af
Piotr Wronarowicz <gawrone@interia.pl>
2018-03-11 20:30:58 +0100

    Finished 9.6

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 // scripts.js
-button.addEventListener(click, onclick);
+var add = document.getElementById('addElem');
 var list = document.getElementById('list');
-var add = document.getElementById('<button>');
-add.addEventListener('click', function() {});
-// tu nie wiem jak zastosowac tę funkcję i jaka struktura - co w czym
-var element = document.createElement('li');
-element.innerHTML = 'item';
-var /*tu nie wiem co*/ = document.getElementsByTagName(/*tu też nie wiem co*/).length;
-list.appendChild(element);
+var listLength = 0;
+add.addEventListener('click', function() {
+    listLength++;
+    var element = document.createElement('li');
+    element.innerHTML = "element " + listLength;
+    list.appendChild(element);
+});
