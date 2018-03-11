137a26b215e9ac9a008c63f634d71666f4bc040a
Piotr Wronarowicz <gawrone@interia.pl>
2018-03-11 19:06:02 +0100

    Finished 9.4

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,12 @@
 // scripts.js
-function drawTree(5) {
-    for (var i=0; i<=5; i++) {
-        star = "";
-        for(var j = 0; j < n; j++) {
-            if (j<=i) {
-            var star += '*';
-              console.log(star);
-            }
+function drawTree(h) {
+    for ( i=0; i<=h; i++) {
+        var star = "";
+        for(var j = 0; j < i; j++) {
+            star += "*";
+        }
+        console.log(star);
     }
 }
+console.log(drawTree(7));
+//w konsoli pod choinką pojawia się "undefined"
