e3745721cb4946a25163511f8db69bce69bf0f39
Piotr Wronarowicz <gawrone@interia.pl>
2018-03-11 20:14:06 +0100

    Finished 9.5

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 // scripts.js
-document.getElementsByClassName('button');
-// to ma pobierać elementy, ale jak teraz "przypisać do zmiennej"?
-for (i = 0; i < button.lenght; i++) {
-    text += button[i] + "<br>"
-// spróbowałem tak, ale w sumie nie chodzi o tekst 'button' tylko o klasę, więc nie wiem co zrobić
+var withButton = document.getElementsByClassName('button');
+console.log(withButton);
+for ( var i=0; i <= withButton.length; i++ ) {
+console.log( withButton[i].innerText);
+};
